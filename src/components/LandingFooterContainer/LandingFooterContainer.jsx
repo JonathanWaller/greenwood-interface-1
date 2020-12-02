7c93127cac961db7cc5a018ec7c751a2cac01336
Brandon McFarland <branmcf@gmail.com>
2020-12-02 18:37:59 -0500

    Added link to discord on landing page

--- src/components/LandingFooterContainer/LandingFooterContainer.jsx
+++ src/components/LandingFooterContainer/LandingFooterContainer.jsx
@@ -3,6 +3,7 @@ import { GitHub, Twitter } from 'react-feather';
 import './LandingFooterContainer.css'
 import { Link } from "react-router-dom";
 import AppContext from '../../contexts/AppContext';
+import discord from '../../assets/images/discord_icon.png';
 
 export default class LandingFooterContainer extends Component { 
   static contextType = AppContext;
@@ -23,7 +24,8 @@ export default class LandingFooterContainer extends Component {
                     {this.context.isDesktop && <li className="va-middle-bg-transparent filled-footer-landing-icon"><Link to="/terms" title="Terms" style={{color: "#66676F"}}>Terms</Link></li>}
                     {this.context.isDesktop && <li className="va-middle-bg-transparent filled-footer-landing-icon"><Link to="/privacy" title="Privacy" style={{color: "#66676F"}}>Privacy</Link></li>}
                     <li><a href="https://github.com/greenwood-finance" title="Greenwood GitHub"><span><GitHub className="va-middle-bg-transparent filled-footer-landing-icon"/></span></a></li>
-                    <li className="footer-last-li"><a href="https://twitter.com/GreenwoodDeFi" title="Greenwood Twitter"><span><Twitter className="va-middle-bg-transparent filled-footer-landing-icon" /></span></a></li>
+                    <li><a href="https://twitter.com/GreenwoodDeFi" title="Greenwood Twitter"><span><Twitter className="va-middle-bg-transparent filled-footer-landing-icon" /></span></a></li>
+                    <li className="footer-last-li"><a href="https://discord.com/invite/dxejH7fAxr" title="Greenwood Discord"><span><img className="va-middle-bg-transparent filled-footer-landing-image" src={discord} alt="discord"/></span></a></li>
                 </ul>
             </div>
         </footer-landing>
