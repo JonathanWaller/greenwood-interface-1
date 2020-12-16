2b93f015db3c0e8b8653047c12afbaaa82e1bd26
Brandon McFarland <branmcf@gmail.com>
2020-12-15 19:58:52 -0500

    Updated twitter handle

--- src/components/LandingFooterContainer/LandingFooterContainer.jsx
+++ src/components/LandingFooterContainer/LandingFooterContainer.jsx
@@ -24,7 +24,7 @@ export default class LandingFooterContainer extends Component {
                     {this.context.isDesktop && <li className="va-middle-bg-transparent filled-footer-landing-icon"><Link to="/terms" title="Terms" style={{color: "#66676F"}}>Terms</Link></li>}
                     {this.context.isDesktop && <li className="va-middle-bg-transparent filled-footer-landing-icon"><Link to="/privacy" title="Privacy" style={{color: "#66676F"}}>Privacy</Link></li>}
                     <li><a href="https://github.com/greenwood-finance" title="Greenwood GitHub"><span><GitHub className="va-middle-bg-transparent filled-footer-landing-icon"/></span></a></li>
-                    <li><a href="https://twitter.com/GreenwoodDeFi" title="Greenwood Twitter"><span><Twitter className="va-middle-bg-transparent filled-footer-landing-icon" /></span></a></li>
+                    <li><a href="https://twitter.com/GreenwoodLabs" title="Greenwood Twitter"><span><Twitter className="va-middle-bg-transparent filled-footer-landing-icon" /></span></a></li>
                     <li className="footer-last-li"><a href="https://discord.com/invite/dxejH7fAxr" title="Greenwood Discord"><span><img className="va-middle-bg-transparent filled-footer-landing-image" src={discord} alt="discord"/></span></a></li>
                 </ul>
             </div>
