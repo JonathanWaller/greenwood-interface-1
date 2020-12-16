6bc77980df17738c86ddb768f3796dfc9dc92351
Brandon McFarland <branmcf@gmail.com>
2020-12-15 20:02:19 -0500

    Updated twitter handle

--- src/components/FooterContainer/FooterContainer.jsx
+++ src/components/FooterContainer/FooterContainer.jsx
@@ -18,7 +18,7 @@ export default class FooterContainer extends Component {
             <div>
                 <ul className="footer__nav__links">
                     <li><a href="https://github.com/greenwood-finance" title="Greenwood GitHub"><span><GitHub className="va-middle-bg-transparent filled-footer-icon"/></span></a></li>
-                    <li><a href="https://twitter.com/GreenwoodDeFi" title="Greenwood Twitter"><span><Twitter className="va-middle-bg-transparent filled-footer-icon" /></span></a></li>
+                    <li><a href="https://twitter.com/GreenwoodLabs" title="Greenwood Twitter"><span><Twitter className="va-middle-bg-transparent filled-footer-icon" /></span></a></li>
                     <li className="footer-last-li"><a href="https://discord.com/invite/dxejH7fAxr" title="Greenwood Discord"><span><img className="va-middle-bg-transparent filled-footer-landing-image" src={discord} alt="discord"/></span></a></li>
                 </ul>
             </div>
